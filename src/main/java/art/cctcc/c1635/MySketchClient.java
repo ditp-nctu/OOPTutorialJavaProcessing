f048e508e8b0305bb953ec5896a57b16435df90d
Jonathan Chang, Chun-yien <ccy@musicapoetica.org>
2020-12-17 18:09:51 +0800

    client-server demo.

---
+++ src/main/java/art/cctcc/c1635/MySketchClient.java
@@ -0,0 +1,88 @@
+package art.cctcc.c1635;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import processing.core.*;
+import static processing.core.PConstants.RGB;
+import static processing.core.PConstants.RIGHT;
+
+/**
+ *
+ * @author Jonathan Chang, Chun-yien <ccy@cctcc.art>
+ */
+public class MySketchClient extends MySketch {
+
+   List<MyObject> pool = new ArrayList<>();
+
+   @Override
+   public void settings() {
+      fullScreen();
+   }
+
+   @Override
+   public void setup() {
+      colorMode(RGB);
+      img = loadImage(getClass().getResource("/nctu.png").getFile());
+   }
+   HttpClient client = HttpClient.newHttpClient();
+
+   @Override
+   public void draw() {
+      if (pool.size() < AMOUNT) {
+
+         HttpRequest request = HttpRequest.newBuilder()
+                 .uri(URI.create(String.format("http://localhost:8001/?%s=%d&%s=%d",
+                         "MIN_SIZE", (int) MIN_SIZE,
+                         "MAX_SIZE", (int) MAX_SIZE)))
+                 .build();
+         var s = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                 .thenApply(HttpResponse::body)
+                 .thenAccept(body -> {
+                    var result = parseJSONObject(body);
+                    var x = result.getFloat("x");
+                    var y = result.getFloat("y");
+                    var size = result.getFloat("size");
+                    var color = result.getInt("color");
+                    System.out.printf("%f,%f,%f,%d\n", x, y, size, color);
+                    pool.add(MyObject.getInstance(x, y, size, color));
+                 })
+                 .join();
+      }
+      background(bgColor);
+      for (MyObject obj : pool) {
+         obj.paint(this);
+         obj.move(random(-DELTA_MOVE / width, DELTA_MOVE / width),
+                 random(-DELTA_MOVE / height, DELTA_MOVE / height));
+         obj.resize(random(-DELTA_SIZE, DELTA_SIZE));
+      }
+      if (mousePressed) {
+         fill(255);
+         circle(width / 2, height / 2, img.pixelWidth * 1.5f);
+         image(img, (width - img.pixelWidth) / 2,
+                 (height - img.height) / 2);
+         if (mouseButton == RIGHT) {
+            noLoop();
+         } else if (isClicked == false) {
+            isClicked = true;
+            bgColor = color(random(200, 255), random(200, 255), random(200, 255));
+         }
+      }
+   }
+
+   @Override
+   public void mouseReleased() {
+      isClicked = false;
+   }
+
+   public static void main(String[] args) {
+      System.setProperty("sun.java2d.uiScale", "1.0");
+      PApplet.main("art.cctcc.c1635.MySketchClient");
+   }
+}
---
+++ src/main/java/art/cctcc/c1635/server/ServerMain.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package art.cctcc.c1635.server;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.concurrent.Executors;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import processing.core.PApplet;
+import processing.data.JSONObject;
+
+/**
+ *
+ * @author Jonathan Chang, Chun-yien <ccy@musicapoetica.org>
+ */
+public class ServerMain {
+
+   static PApplet p = new PApplet();
+
+   public static void main(String[] args) throws IOException {
+
+      HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8001), 0);
+      var logger = Logger.getGlobal();
+      server.createContext("/", (HttpExchange exchange) -> {
+         var params = Arrays.stream(exchange.getRequestURI().getQuery().split("&"))
+                 .map(s -> s.split("="))
+                 .collect(Collectors.toMap(p -> p[0], p -> p[1]));
+         var min_size = Integer.parseInt(params.get("MIN_SIZE"));
+         var max_size = Integer.parseInt(params.get("MAX_SIZE"));
+         var x = p.random(1);
+         var y = p.random(1);
+         var size = p.random(min_size, max_size);
+         var color = p.color(p.random(150, 250), p.random(150, 250), p.random(150, 250));
+         var response = new JSONObject();
+         response.put("x", (float) Math.random());
+         response.put("y", (float) Math.random());
+         response.put("size", (float) p.random(min_size, max_size));
+         response.put("color", color);
+         System.out.println("response = " + response.toString());
+         try (OutputStream outputStream = exchange.getResponseBody()) {
+            exchange.sendResponseHeaders(200, response.toString().length());
+            outputStream.write(response.toString().getBytes());
+            outputStream.flush();
+         }
+      });
+
+      server.setExecutor(Executors.newFixedThreadPool(10));
+      server.start();
+
+      logger.info(" Server started on port 8001");
+   }
+}
